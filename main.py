cf85841e436a06c731bfbbef79378596069e335f
Michelle <mpuschkarow@web.de>
2025-10-07 12:29:47 +0200

    set up for screen and key event listeners

---
+++ main.py
@@ -0,0 +1,36 @@
+from turtle import Screen
+import time
+
+screen = Screen()
+
+def restart():
+    global screen
+    screen.clearscreen()
+    main()
+    print("restarted")
+
+def main():
+    global screen
+    screen.setup(width=600, height=600) #keyword arguments
+    screen.bgcolor("black")
+    screen.title("My Snake Game")
+    screen.tracer(0)
+    game_over = False
+
+    #Event Listener
+    screen.listen()
+    screen.onkey(key="Up", fun=snake.move_up)
+    screen.onkey(key="Down", fun=snake.move_down)
+    screen.onkey(key="Left", fun=snake.move_left)
+    screen.onkey(key="Right", fun=snake.move_right)
+    screen.onkey(key="r", fun=restart)
+
+    #Game running
+    while not game_over:
+        screen.update()
+        time.sleep(0.1)
+        snake.move()
+
+    screen.exitonclick()
+
+main()
